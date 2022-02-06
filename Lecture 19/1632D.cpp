7b0c946fa24c910a840fd062f2b84acad231074e
Shrey Dubey <wshrey09@gmail.com>
2022-02-06 15:27:51 +0530

    d

---
+++ Lecture 19/1632D.cpp
@@ -0,0 +1,191 @@
+
+
+//Shrey Dubey
+
+
+#include    <iostream>
+#include    <string>
+#include    <algorithm>
+#include    <map>
+#include    <unordered_map>
+#include    <vector>
+#include    <set>
+#include    <list>
+#include    <iomanip>
+#include    <queue>
+#include    <stack>
+#include    <math.h>
+#include    <climits>
+#include    <bitset>
+#include    <cstring>
+#include    <numeric>
+#include    <array>
+#include    <deque>
+#include    <cstdlib>
+#include    <cstdio>
+#include    <stdlib.h>
+#include    <cerrno>
+#include    <ctime>
+#include    <unordered_set>
+#include    <cstring>
+#include    <cmath>
+#include    <random>
+#include    <functional>
+#include    <cassert>
+#include    <bitset>
+#include    <chrono>
+
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define prDouble(x) cout<<fixed<<setprecision(10)<<x //to print decimal numbers
+#define pb push_back
+#define ff first
+#define sec second
+#define bct(x) __builtin_popcountll(x)
+#define umap unordered_map
+#define mp make_pair
+#define KOBE ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fo(n) for(ll i = 0; i<n; i++)
+#define fnd(stl, data) find(stl.begin(), stl.end(), data)
+#define forn(x,n) for(ll x = 0; x<n; x++)
+#define imax INT_MAX
+#define lmax LLONG_MAX
+#define imin INT_MIN
+#define lmin LLONG_MIN
+#define vi vector<int>
+#define vl vector<ll>
+#define vp vector<pair<ll,ll> >
+#define vs vector<string>
+#define vb vector<bool>
+#define pr(t) cout<<t<<"\n"
+#define int long long
+#define ql queue<ll>
+#define qp queue<pair<ll,ll> >
+#define endl "\n"
+#define nl cout<<"\n"
+#define re cin >>
+#define pll pair<ll,ll>
+#define FOR(a,b) for(ll i = a; i<=b; i++)
+#define all(x) x.begin(),x.end()
+#define LG 20
+#define INF 1e18
+#define p 1000000007
+
+// ll dx[] = {1,0,-1,0};
+// ll dy[] = {0,1,0,-1};
+
+const ll mod = 1e9 + 7;
+
+ll cl(ld a){
+    if(a>(ll) a){
+        return (ll)a+1;
+    }
+    else{
+        return (ll)a;
+    }
+}
+
+ll flr(ld a){
+    if(a < 0.0){
+        return (ll) a - 1;
+    }
+    return (ll) a;
+}
+
+ll gcd(ll a, ll b){
+    if(b == 0) return a;
+    else return gcd(b,a%b);
+}
+
+ll pw(ll n, ll k){
+    if(k == 0) return 1;
+    ll t = pw(n,k/2);
+    ll ans = (t*t)%mod;
+    if(k % 2) return (ans*n)%mod;
+    else return ans;
+}
+
+//code starts here
+
+const ll M = 2e5+100;
+
+vector<ll> st(4*M+1);
+ll a[M],n;
+
+void build(ll v, ll tl, ll tr){
+    if(tl == tr){
+        st[v] = a[tl];
+        return;
+    }
+    ll tm = (tl + tr)/2;
+    build(2*v,tl,tm);
+    build(2*v+1,tm+1,tr);
+    st[v] = gcd(st[2*v],st[2*v+1]);
+}
+
+ll query(ll v, ll tl, ll tr, ll l, ll r){
+    if(tl > r || tr < l) return 0;
+    if(tl >= l && tr <= r) return st[v];
+    ll tm = (tl + tr)/2;
+    return gcd(query(2*v,tl,tm,l,r),query(2*v+1,tm+1,tr,l,r));
+}
+
+
+void solve(){
+    re n;
+    fo(n) re a[i];
+    ll l = 0;
+    ll ans = 0;
+    build(1,0,n-1);
+    for(ll i = 0; i<n; i++){
+        ll s = l, e = i;
+        while(s <= e){
+            ll mid = (s+e)/2;
+            ll g = query(1,0,n-1,mid,i);
+            if(g == i-mid+1){
+                ans++;
+                l = i+1; //to avoid updating, we shift the left pointer ahead
+                break;
+            }
+            if(g > i-mid+1){
+                e = mid-1;
+            }else if(g < i-mid+1){
+                s = mid+1;
+            }
+        }
+        cout<<ans<<" ";
+    }
+    nl;
+}
+
+int32_t main(){
+    KOBE;
+    ll t;
+    t = 1;
+    // re t;
+    while(t--) solve();
+}
+
+
+//common errors
+// row - n, col - m always and loop var
+// see the freq of numbers carefully
+// see if there's array overflow
+// use map for large inputs
+// always check for n = 1 condition
+// check loop starting and end points :(
+//when dividing with mod, use mod inverse
+
+//problem ideas
+//check piegonhole wherever possible
+//there might be many instances of limited answers like 0,1,2 only
+// see suffix and prefix
+//don't be obsessed with binary search
+// try to find repeating pattern in matrices
---
+++ Lecture 19/tries.cpp
@@ -0,0 +1,211 @@
+
+
+//Shrey Dubey
+
+
+#include    <iostream>
+#include    <string>
+#include    <algorithm>
+#include    <map>
+#include    <unordered_map>
+#include    <vector>
+#include    <set>
+#include    <list>
+#include    <iomanip>
+#include    <queue>
+#include    <stack>
+#include    <math.h>
+#include    <climits>
+#include    <bitset>
+#include    <cstring>
+#include    <numeric>
+#include    <array>
+#include    <deque>
+#include    <cstdlib>
+#include    <cstdio>
+#include    <stdlib.h>
+#include    <cerrno>
+#include    <ctime>
+#include    <unordered_set>
+#include    <cstring>
+#include    <cmath>
+#include    <random>
+#include    <functional>
+#include    <cassert>
+#include    <bitset>
+#include    <chrono>
+
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define prDouble(x) cout<<fixed<<setprecision(10)<<x //to print decimal numbers
+#define pb push_back
+#define ff first
+#define sec second
+#define bct(x) __builtin_popcountll(x)
+#define umap unordered_map
+#define mp make_pair
+#define KOBE ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fo(n) for(ll i = 0; i<n; i++)
+#define fnd(stl, data) find(stl.begin(), stl.end(), data)
+#define forn(x,n) for(ll x = 0; x<n; x++)
+#define imax INT_MAX
+#define lmax LLONG_MAX
+#define imin INT_MIN
+#define lmin LLONG_MIN
+#define vi vector<int>
+#define vl vector<ll>
+#define vp vector<pair<ll,ll> >
+#define vs vector<string>
+#define vb vector<bool>
+#define pr(t) cout<<t<<"\n"
+#define int long long
+#define ql queue<ll>
+#define qp queue<pair<ll,ll> >
+#define endl "\n"
+#define nl cout<<"\n"
+#define re cin >>
+#define pll pair<ll,ll>
+#define FOR(a,b) for(ll i = a; i<=b; i++)
+#define all(x) x.begin(),x.end()
+#define LG 20
+#define INF 1e18
+#define p 1000000007
+
+// ll dx[] = {1,0,-1,0};
+// ll dy[] = {0,1,0,-1};
+
+const ll mod = 1e9 + 7;
+
+ll cl(ld a){
+    if(a>(ll) a){
+        return (ll)a+1;
+    }
+    else{
+        return (ll)a;
+    }
+}
+
+ll flr(ld a){
+    if(a < 0.0){
+        return (ll) a - 1;
+    }
+    return (ll) a;
+}
+
+ll gcd(ll a, ll b){
+    if(b == 0) return a;
+    else return gcd(b,a%b);
+}
+
+ll pw(ll n, ll k){
+    if(k == 0) return 1;
+    ll t = pw(n,k/2);
+    ll ans = (t*t)%mod;
+    if(k % 2) return (ans*n)%mod;
+    else return ans;
+}
+
+//code starts here
+
+class node{
+    public:
+        char data;
+        unordered_map<char,node*> children;
+        bool terminal;
+        node(char a){
+            data = a;
+            terminal = false;
+        }
+};
+
+class Trie{
+    public:
+    node* root;
+    Trie(){
+        root = new node('\0');
+    }
+    void insert(string s){
+        node *temp = root;
+        for(ll i = 0; i<s.length(); i++){
+            if(temp->children.count(s[i]) == 0){
+                temp->children[s[i]] = new node(s[i]);
+            }
+            temp = temp->children[s[i]];
+        }
+        temp->terminal = true;
+    }
+
+    bool search(string s){
+        node *temp = root;
+        for(ll i = 0; i<s.length(); i++){
+            if(temp->children.count(s[i]) == 0){
+                return false;
+            }
+            temp = temp->children[s[i]];
+        }
+        if(temp->terminal) return true;
+        else return false;
+    }
+};
+
+void solve(){
+    ll n;
+    re n;
+    Trie T;
+    fo(n){
+        string s; re s;
+        T.insert(s);
+    }
+    ll q; re q;
+    while(q--){
+        string s;
+        re s;
+        if(T.search(s)) pr("PRESENT");
+        else pr("NOT PRESENT");
+    }
+
+}
+
+int32_t main(){
+    KOBE;
+    ll t;
+    t = 1;
+    // re t;
+    while(t--) solve();
+}
+
+
+//common errors
+// row - n, col - m always and loop var
+// see the freq of numbers carefully
+// see if there's array overflow
+// use map for large inputs
+// always check for n = 1 condition
+// check loop starting and end points :(
+//when dividing with mod, use mod inverse
+
+//problem ideas
+//check piegonhole wherever possible
+//there might be many instances of limited answers like 0,1,2 only
+// see suffix and prefix
+//don't be obsessed with binary search
+// try to find repeating pattern in matrices
+
+/*
+4
+these
+them
+there
+him
+3
+them
+the
+these
+*/
